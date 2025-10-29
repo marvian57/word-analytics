aa689ede3757ecba05861a51f896bae70b9cd953
LaoHai <blankkid22@gmail.com>
2025-10-29 22:28:01 +0200

    update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Footer() {
   return (
     <footer className="footer">
-      <small>&copy; Copyright by ByteGrad. All rights reserved.</small>
+      <small>&copy; All rights reserved.</small>
       <small>Last checked limits: 17 days ago.</small>
     </footer>
   );
